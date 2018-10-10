7a0fb5a7a50cf4b45c943a623f73ad2e33d1193c
Przemek Paszkiewicz <ppaszkiewicz@squiz.pl>
2018-10-10 09:03:55 +0200

    Updates

---
+++ precache-manifest.493be513e747eef6355df99891a738a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "3f7ebc9bea7e0c339450",
+    "url": "/react/static/js/main.3f7ebc9b.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "3f7ebc9bea7e0c339450",
+    "url": "/react/static/css/main.d0d28bf4.chunk.css"
+  },
+  {
+    "revision": "ac409f159ca6b2b8d30e956e895c4245",
+    "url": "/react/index.html"
+  }
+];
